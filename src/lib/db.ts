9f0df7817c7dfad54119e375d10a9f6584fca6b6
lucast <trevensolidasilvafelixlucas@gmail.com>
2026-05-09 19:08:03 -0300

    More Fixes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,8 @@
 import mysql from 'mysql2/promise';
 
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
