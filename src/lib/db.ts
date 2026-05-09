a260aeba3462a2010fc1927741864c607d8bd8b9
lucast <trevensolidasilvafelixlucas@gmail.com>
2026-05-09 19:04:01 -0300

    Fix Railway database

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', 
-  password: '', 
-  database: 'musicalstats',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
